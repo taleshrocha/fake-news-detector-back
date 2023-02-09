049900845dab100b12047db8c4f777cca6ff2b1f
taleshrocha <taleshrochaz@gmail.com>
2023-02-09 06:40:14 -0300

    Base project

---
+++ src/main/java/com/fakenewsdetector/demo/FakeNewsDetectorApplication.java
@@ -0,0 +1,13 @@
+package com.fakenewsdetector.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakeNewsDetectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FakeNewsDetectorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fakenewsdetector/demo/FakeNewsDetectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fakenewsdetector.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakeNewsDetectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
