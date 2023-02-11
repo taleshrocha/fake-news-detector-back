6e8a38cf14ef3317b02e09d65177f01a12c306ec
taleshrocha <taleshrochaz@gmail.com>
2023-02-11 14:58:25 -0300

    NewsController

---
+++ src/main/java/com/fakenewsdetector/controller/NewsController.java
@@ -0,0 +1,73 @@
+package com.fakenewsdetector.controller;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.IanaLinkRelations;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fakenewsdetector.assembler.NewsModelAssembler;
+import com.fakenewsdetector.dao.NewsRepository;
+import com.fakenewsdetector.exception.NewsNotFoundException;
+import com.fakenewsdetector.model.News;
+
+@RestController
+public class NewsController {
+
+  private final NewsRepository newsRepository;
+  private final NewsModelAssembler newsAssembler;
+
+  NewsController(NewsRepository newsRepository, NewsModelAssembler newsAssembler) {
+    this.NewsRepository = newsRepository;
+    this.NewsAssembler = newsAssembler;
+  }
+
+  @GetMapping("/news")
+  public CollectionModel<EntityModel<News>> all() {
+
+    List<EntityModel<News>> news = NewsRepository.findAll().stream()
+        .map(newsAssembler::toModel)
+        .collect(Collectors.toList());
+
+    return CollectionModel.of(news,
+        linkTo(methodOn(NewsController.class).all()).withSelfRel());
+
+  }
+
+  @GetMapping("/news/{id}")
+  public EntityModel<News> one(@PathVariable Long id) {
+
+    News news = NewsRepository.findById(id)
+        .orElseThrow(() -> new NewsNotFoundException(id));
+
+    return NewsAssembler.toModel(news);
+  }
+
+  @PostMapping("/news")
+  public ResponseEntity<?> newNews(@RequestBody News newNews) {
+    EntityModel<News> entityModel = newsAssembler.toModel(NewsRepository.save(newNews));
+
+    return ResponseEntity
+        .created(entityModel.getRequiredLink(IanaLinkRelations.SELF).toUri())
+        .body(entityModel);
+  }
+
+  @DeleteMapping("/news/{id}")
+  public ResponseEntity<?> removeNews(@PathVariable Long id) {
+
+    NewsRepository.deleteById(id);
+
+    return ResponseEntity.noContent().build();
+  }
+}
+
